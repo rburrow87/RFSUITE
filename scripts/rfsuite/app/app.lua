efea4fe8f590b6f7c1e4a7f8c17b4ca48769c556
Rob Thomson <rob@marotori.com>
2024-09-29 14:06:49 +0100

    Missing check for bgtask not running

--- scripts/rfsuite/app/app.lua
+++ scripts/rfsuite/app/app.lua
@@ -793,7 +793,7 @@ function app.wakeupUI()
 
                         if app.triggers.badMspVersionDisplay == false then
                                 local message
-                                if rfsuite.bg.msp.backgroundMsp ~= true then
+                                if not rfsuite.bg.msp or rfsuite.bg.msp.backgroundMsp ~= true then
                                    message = "Please enable the background task."
                                 elseif app.getRSSI() == 0 then
                                         message = "Unable to establish a link to the flight controller"
